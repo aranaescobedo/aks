c710e4a7fc8dc467bba697caff47b275163304e0
Alexander Arana Escobedo <117573697+aranaescobedo@users.noreply.github.com>
2023-03-28 21:48:17 +0200

    script for creating new node pool in the aks cluster (#1)

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# aks
+# AKS (Azure Kubernetes Service)
+This folder contains scripts that are used to interact with Azure Kubernetes Service (AKS).
+
+## Prerequisites
+
+- [A valid Azure account][azure-account]
+- [Azure CLI][azure-cli]
+- [kubectl][kubectl]
+
+## Usage
+Each script in this folder is designed to perform a specific task with AKS. Before running a script, make sure to replace any placeholders with your own information.
+
+## Script Descriptions
+
+- **[create-node-pool.ps1]**:  Creates a new node pool on your AKS-cluster.
+
+## Disclaimer
+Please note that this is provided as-is and may not suit all use cases. Use at your own discretion and make sure to thoroughly test before deployment in a production environment.
+
+[azure-account]: https://azure.microsoft.com/en-us/free
+[azure-cli]: https://docs.microsoft.com/en-us/cli/azure
+[create-node-pool.ps1]:create-node-pool.ps1
+[kubectl]:https://kubernetes.io/docs/tasks/tools/
---
+++ create-node-pool.ps1
@@ -0,0 +1,58 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]
+    $aksName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $aksResourceGroup,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $aksVersion,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $aksVnetName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $networkResourceGroup,
+    [Parameter(Mandatory = $true)]
+    [ValidateLength(1,12)] #Nodepool name can only contain at most 12 characters
+    [string]
+    $nodePoolName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $subnetName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $subscriptionName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $vmSize,
+    [Parameter(Mandatory = $true)]
+    [string]
+    [ValidateSet("1", "1 2", "1 2 3")]
+    $zones
+)
+
+"[*] Setting subscription to $subscriptionName"
+az account set --subscription $subscriptionName
+
+"[*] Getting subnet id for $subnetName"
+$subnetId = (az network vnet subnet show --resource-group $networkResourceGroup --vnet-name $aksVnetName --name $subnetName | ConvertFrom-Json).id
+
+"[*] Creating node pool: $nodePoolName..."
+az aks nodepool add `
+    --resource-group $aksResourceGroup `
+    --cluster-name $aksName `
+    --name $nodePoolName `
+    --enable-cluster-autoscaler `
+    --node-count 1 `
+    --min-count 1 `
+    --max-count 2 `
+    --kubernetes-version $aksVersion `
+    --os-sku "Ubuntu" `
+    --os-type "Linux" `
+    --max-pods 30 `
+    --mode "User" `
+    --node-vm-size $vmSize `
+    --vnet-subnet-id $subnetId `
+    --zones $zones
