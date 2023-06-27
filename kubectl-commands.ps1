3b63cf23980f83b137a859838eb7624143887bf6
Alexander Arana Escobedo <117573697+aranaescobedo@users.noreply.github.com>
2023-06-27 22:46:19 +0200

    Update kubectl-commands.ps1

--- kubectl-commands.ps1
+++ kubectl-commands.ps1
@@ -6,3 +6,6 @@ kubectl top nodes | Where-Object { $_ -like "*<NODE_NAME>*" }
 
 #Displays real-time CPU and memory usage statistics for the pods within the specified namespace in a Kubernetes cluster.
 kubectl top pods -n <NAMESPACE_NAME>
+
+#Provides detailed information about a node in the Kubernetes cluster, including its labels, capacity, usage, conditions, and other relevant details.
+kubectl describe node <NODE_NAME>
