6bfa8bdbee4af93e020b3d661257a8847a857dfd
Alexander Arana Escobedo <117573697+aranaescobedo@users.noreply.github.com>
2023-10-03 23:32:06 +0200

    Update delete-namespace.ps1

--- delete-namespace.ps1
+++ delete-namespace.ps1
@@ -9,7 +9,7 @@ kubectl proxy
 #In a new terminal session, run the following command:
 curl -k -H "Content-Type: application/json" -X PUT --data-binary "@temp.json" http://127.0.0.1:8001/api/v1/namespaces/<NAMESPACE_NAME>/finalize
 
-# For Linux or Bash:
+#For Linux or Bash:
 #curl -k -H "Content-Type: application/json" -X PUT --data-binary @temp.json http://127.0.0.1:8001/api/v1/namespaces/<NAMESPACE_NAME>/finalize
 
 #Once the deletion is complete, don't forget to stop the kubectl proxy by pressing Ctrl + C.
