53cf0717d61ec95c5d60506c1a08cf5e51c5baeb
Alexander Arana Escobedo <117573697+aranaescobedo@users.noreply.github.com>
2023-11-16 23:21:41 +0100

    Update script.ps1

--- backup-mounted -volume/manifests/script.ps1
+++ backup-mounted -volume/manifests/script.ps1
@@ -25,80 +25,87 @@ az group create --name $kvResourceGroup --location $location
 
 "[*] Create Key Vault"
 $keyVaultId = az keyvault create `
---name $kvName `
---resource-group $kvResourceGroup `
---enable-rbac-authorization true `
---location $location `
---enabled-for-disk-encryption true `
---query "id" -o tsv `
---enable-purge-protection false #This should be 'true' when used in real prod scenarios.
+              --name $kvName `
+              --resource-group $kvResourceGroup `
+              --enable-rbac-authorization true `
+              --location $location `
+              --enabled-for-disk-encryption true `
+              --query "id" -o tsv `
+              --enable-purge-protection false #This should be 'true' when used in real prod scenarios.
 
 "[*] Give yourself the Key Vault Administrator permission"
 az role assignment create `
---assignee $email `
---role "Key Vault Administrator" `
---scope $keyVaultId
+  --assignee $email `
+  --role "Key Vault Administrator" `
+  --scope $keyVaultId
 
 "[*] Create Key"
 $keyUrl = az keyvault key create `
---subscription $subscriptionName `
---vault-name $kvName `
---name $keyName `
---kty RSA `
---size 2048 `
---query "key.kid" -o tsv
+          --subscription $subscriptionName `
+          --vault-name $kvName `
+          --name $keyName `
+          --kty RSA `
+          --size 2048 `
+          --query "key.kid" -o tsv
 
 "[*] Create disk encryption set"
 $diskEncryptionSetId = az disk-encryption-set create `
---resource-group $kvResourceGroup `
---name $desName `
---key-url $keyUrl `
---source-vault $kvName `
---query "id" -o tsv
+  --resource-group $kvResourceGroup `
+  --name $desName `
+  --key-url $keyUrl `
+  --source-vault $kvName `
+  --query "id" -o tsv
 
 "[*] Get disk encryption set identity"
 $principalId = az disk-encryption-set identity show `
---name $desName `
---resource-group $kvResourceGroup `
---query "principalId" -o tsv
+              --name $desName `
+              --resource-group $kvResourceGroup `
+              --query "principalId" -o tsv
 
 "[*] Give disk encyption set access to Key Vault"
 az role assignment create `
---assignee $principalId `
---role "Key Vault Crypto Service Encryption User" `
---scope $keyVaultId
-#--role "Key Vault Crypto User" `CHECK IF YOU ARE GIVING THE RIGHT ACCESS TO THE KEY VAULT..
+  --assignee $principalId `
+  --role "Key Vault Crypto Service Encryption User" `
+  --scope $keyVaultId
+  #--role "Key Vault Crypto User" `CHECK IF YOU ARE GIVING THE RIGHT ACCESS TO THE KEY VAULT..
 
 #"[*] Create resource group for the AKS"
 #az group create --name $aksResourceGroup --location $location
+
+"[*] Create a user assigned identity"
+$clientId = az identity create `
+    --name $userAssignedIdentity `
+    --resource-group $aksResourceGroup `
+    --query "clientId" -o tsv
 
 "[*] Create AKS"
 #Attn: Encryption of OS disk with customer-managed keys can only be enabled when creating an AKS cluster!
 #When creating the cluster you already get Azure Disks CSI driver enabled.
 az aks create `
---resource-group $kvResourceGroup `
---name $aksName `
---kubernetes-version 1.26 `
---node-count 1 `
---location $location `
---network-plugin azure `
---node-osdisk-diskencryptionset-id $diskEncryptionSetId `
---node-osdisk-type Managed
+  --resource-group $kvResourceGroup `
+  --name $aksName `
+  --assign-identity $clientId `
+  --kubernetes-version 1.26 `
+  --node-count 1 `
+  --location $location `
+  --network-plugin azure `
+  --node-osdisk-diskencryptionset-id $diskEncryptionSetId `
+  --node-osdisk-type Managed
 
 "[*] Update AKS with with disk driver"
 #Not possible to run --enable-disk-driver with az aks create
 az aks update `
---resource-group $kvResourceGroup `
---name $aksName `
---enable-disk-driver
+  --resource-group $kvResourceGroup `
+  --name $aksName `
+  --enable-disk-driver
 
 "[*] Create Backup Vault" 
 az dataprotection backup-vault create `
---resource-group $kvResourceGroup `
---vault-name $backupVaultName `
---location $location `
---type SystemAssigned
---storage-settings datastore-type="VaultStore"type="LocallyRedundant"
+  --resource-group $kvResourceGroup `
+  --vault-name $backupVaultName `
+  --location $location `
+  --type SystemAssigned
+  --storage-settings datastore-type="VaultStore"type="LocallyRedundant"
 
 "[*] Get policy template"
 az dataprotection backup-policy get-default-policy-template --datasource-type AzureDisk > policy.json
