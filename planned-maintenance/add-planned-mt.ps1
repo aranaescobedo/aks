f3fea10bd3896e6d52a35087df7a06eccbfd6070
Alexander Arana E <alexander@innovate-it.se>
2024-04-18 22:00:51 +0200

    updating the scripts

--- planned-maintenance/add-planned-mt.ps1
+++ planned-maintenance/add-planned-mt.ps1
@@ -23,26 +23,29 @@ Set-Location "$getRootDir\planned-maintenance"
 
 #Variables
 $aksName = "aks-cluster-$($environment)-we-01"
+$location = "west europe"
 $resouceGroup = "rg-cluster-$($environment)-we"
 
 #az login
 az account set --subscription $subscriptionName
 "[*] Set subscription $subscriptionName"
 
-az aks update `
---resource-group $resouceGroup `
---name $aksName `
---auto-upgrade-channel stable
+#Create resource group for the AKS:
+az group create `
+    --name $resouceGroup `
+    --location $location
 
-#Add planned AKS upgrades.
-az aks maintenanceconfiguration add `
+#Create AKS:
+az aks create `
     --resource-group $resouceGroup `
-    --cluster-name $aksName `
-    --name aksManagedAutoUpgradeSchedule `
-    --config-file $environment-auto-upgrade-cluster-sched.json
+    --name $aksName `
+    --node-count 1 `
+    --kubernetes-version 1.28.5 `
+    --location $location `
+    --auto-upgrade-channel stable
 
-#Update Planned AKS upgrades.
-az aks maintenanceconfiguration update `
+#Add Planned AKS upgrades.
+az aks maintenanceconfiguration add `
     --resource-group $resouceGroup `
     --cluster-name $aksName `
     --name aksManagedAutoUpgradeSchedule `
@@ -55,26 +58,20 @@ az aks maintenanceconfiguration add `
     --name aksManagedNodeOSUpgradeSchedule `
     --config-file $environment-auto-upgrade-node-os-sched.json
 
-#Planned node OS images upgrades.
-az aks maintenanceconfiguration update `
-    --resource-group $resouceGroup `
-    --cluster-name $aksName `
-    --name aksManagedNodeOSUpgradeSchedule `
-    --config-file $environment-auto-upgrade-node-os-sched.json
-
 #List maintenance configurations in the cluster.
 az aks maintenanceconfiguration list `
-     --resource-group $resouceGroup `
-     --cluster-name $aksName
+      --resource-group $resouceGroup `
+      --cluster-name $aksName
 
 #Show the details of a maintenance configuration in the cluster.
 az aks maintenanceconfiguration show `
-    --resource-group $resouceGroup `
-    --cluster-name $aksName `
-    --name aksManagedAutoUpgradeSchedule
+     --resource-group $resouceGroup `
+     --cluster-name $aksName `
+     --name aksManagedAutoUpgradeSchedule
 
 #Delete a maintenance configuration in the cluster.
 az aks maintenanceconfiguration delete `
     --resource-group $resouceGroup `
     --cluster-name $aksName `
     --name aksManagedNodeOSUpgradeSchedule
+
